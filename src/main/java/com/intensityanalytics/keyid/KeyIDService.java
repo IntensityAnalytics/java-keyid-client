4cfa9e02a2085681d73185b307c708b00c4d723e
Jonathan Galentine <j.galentine@gmail.com>
2018-04-11 11:49:14 -0600

    Squash merge futuresnexceptions into master.

--- src/main/java/com/intensityanalytics/keyid/KeyIDService.java
+++ src/main/java/com/intensityanalytics/keyid/KeyIDService.java
@@ -1,8 +1,236 @@
 package com.intensityanalytics.keyid;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import okhttp3.Call;
+import okhttp3.HttpUrl;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.net.URLEncoder;
+import java.util.concurrent.CompletionException;
+
 public class KeyIDService
 {
-    public String helloWorld(){
-        return "hello world";
+    private String url;
+    private String license;
+    private OkHttpClient client;
+    public static final MediaType URLENCODED = MediaType.parse("application/x-www-form-urlencoded; charset=utf-8");
+
+    public KeyIDService(String url, String license, int timeoutMs)
+    {
+        this.url = url;
+        this.license = license;
+        client = new OkHttpClient();
+    }
+
+    public String getUrl()
+    {
+        return url;
+    }
+
+    public String getLicense()
+    {
+        return license;
+    }
+
+    public JsonObject encodeJSONProperties(JsonObject data)
+    {
+        JsonObject objEncoded = new JsonObject();
+
+        try
+        {
+            for (Map.Entry<String, JsonElement> jsonTuple : data.entrySet())
+            {
+                String encodeData = URLEncoder.encode(jsonTuple.getValue().getAsString(), "UTF-8");
+                objEncoded.addProperty(jsonTuple.getKey(), encodeData);
+            }
+        }
+        catch (Exception e)
+        {
+            throw new CompletionException(e);
+        }
+
+        return objEncoded;
+    }
+
+    public CompletableFuture<Response> Post(String path, JsonObject data)
+    {
+        data.addProperty("License", license);
+        JsonObject dataEncoded = encodeJSONProperties(data);
+        String dataEncodedJSON = dataEncoded.toString();
+
+        HttpUrl requestUrl = HttpUrl.parse(url).newBuilder()
+                .addPathSegment(path)
+                .build();
+
+        RequestBody body = RequestBody.create(URLENCODED, "=[" + dataEncodedJSON + "]");
+
+        Request request = new Request.Builder()
+                .url(requestUrl)
+                .post(body)
+                .build();
+
+        Call call = client.newCall(request);
+        OkHttpResponseFuture result = new OkHttpResponseFuture(call);
+        call.enqueue(result);
+
+        return result.future;
+    }
+
+    public CompletableFuture<Response> Get(String path, JsonObject data)
+    {
+        HttpUrl requestUrl = HttpUrl.parse(url).newBuilder()
+                .addPathSegment(path)
+                .build();
+
+        for (Map.Entry<String, JsonElement> jsonTuple : data.entrySet())
+        {
+            requestUrl = requestUrl.newBuilder()
+                    .addQueryParameter(jsonTuple.getKey(), jsonTuple.getValue().getAsString())
+                    .build();
+        }
+
+        Request request = new Request.Builder()
+                .url(requestUrl)
+                .build();
+
+        Call call = client.newCall(request);
+        OkHttpResponseFuture result = new OkHttpResponseFuture(call);
+        call.enqueue(result);
+
+        return result.future;
+    }
+
+    public CompletableFuture<Response> TypingMistake(String entityID, String mistype, String sessionID, String source, String action, String tmplate, String page )
+    {
+        JsonObject data = new JsonObject();
+        data.addProperty("EntityID", entityID);
+        data.addProperty("Mistype", mistype);
+        data.addProperty("SessionID", sessionID);
+        data.addProperty("Source", source);
+        data.addProperty("Action", action);
+        data.addProperty("Template", tmplate);
+        data.addProperty("Page", page);
+
+        return Post("typingmistake", data);
+    }
+
+    public CompletableFuture<Response> EvaluateSample(String entityID, String tsData, String nonce)
+    {
+        JsonObject data = new JsonObject();
+        data.addProperty("EntityID", entityID);
+        data.addProperty("tsData", tsData);
+        data.addProperty("Nonce", nonce);
+        data.addProperty("Return", "JSON");
+        data.addProperty("Statistics", "extended");
+
+        return Post("evaluate", data);
+    }
+
+    public CompletableFuture<Response> Nonce(long nonceTime)
+    {
+        JsonObject data = new JsonObject();
+        data.addProperty("type", "nonce");
+        String path = "token/" + nonceTime;
+        return Get(path,data);
+    }
+
+    public CompletableFuture<Response> RemoveToken(String entityID, String tsData)
+    {
+        JsonObject data = new JsonObject();
+        data.addProperty("Type", "remove");
+        data.addProperty("Return", "value");
+        String path = "token/" + entityID;
+
+        return Get(path, data)
+                .thenCompose(response -> {
+                    JsonObject postData = new JsonObject();
+                    String token;
+                    try{
+                        token = response.body().string();
+                    }
+                    catch (Exception e)
+                    {
+                        throw new CompletionException(e);
+                    }
+
+                    postData.addProperty("EntityID", entityID);
+                    postData.addProperty("Token", token);
+                    postData.addProperty("ReturnToken", "True");
+                    postData.addProperty("ReturnValidation", tsData);
+                    postData.addProperty("Type", "remove");
+                    postData.addProperty("Return", "JSON");
+
+                    return Post("token", postData);
+                });
+    }
+
+    public CompletableFuture<Response> RemoveProfile(String entityID, String token)
+    {
+        JsonObject data = new JsonObject();
+        data.addProperty("EntityID", entityID);
+        data.addProperty("Code", token);
+        data.addProperty("Action", "remove");
+        data.addProperty("Return", "JSON");
+
+        return Post("profile", data);
+    }
+
+    public CompletableFuture<Response> SaveToken(String entityID, String tsData){
+        JsonObject data = new JsonObject();
+        data.addProperty("Type", "enrollment");
+        data.addProperty("Return", "value");
+
+        String path = "token/" + entityID;
+        return Get(path, data)
+                .thenCompose(response -> {
+                    JsonObject postData = new JsonObject();
+                    String token;
+                    try{
+                        token = response.body().string();
+                    }
+                    catch (Exception e)
+                    {
+                        throw new CompletionException(e);
+                    }
+
+                    postData.addProperty("EntityID", entityID);
+                    postData.addProperty("Token", token);
+                    postData.addProperty("ReturnToken", "True");
+                    postData.addProperty("ReturnValidation", tsData);
+                    postData.addProperty("Type", "enrollment");
+                    postData.addProperty("Return", "JSON");
+
+                    return Post("token", postData);
+                });
+    }
+
+    public CompletableFuture<Response> SaveProfile(String entityID, String tsData, String code)
+    {
+        JsonObject data = new JsonObject();
+        data.addProperty("EntityID", entityID);
+        data.addProperty("tsData", tsData);
+        data.addProperty("Return", "JSON");
+        data.addProperty("Action", "v2");
+        data.addProperty("Statistics", "extended");
+
+        if (code != "")
+            data.addProperty("Code", code);
+
+        return Post("profile", data);
+    }
+
+    public CompletableFuture<Response> GetProfileInfo(String entityID)
+    {
+        JsonObject data = new JsonObject();
+        String path = "profile/" + entityID;
+        return Get(path, data);
     }
 }
---
+++ src/test/java/com/intensityanalytics/keyid/KeyIDServiceTest.java
@@ -0,0 +1,107 @@
+package com.intensityanalytics.keyid;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class KeyIDServiceTest
+{
+    static String readFile(String path, Charset encoding) throws IOException
+    {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
+    }
+
+    @Test
+    public void testGet() throws Exception
+    {
+        String license = readFile("license.txt", StandardCharsets.UTF_8);
+        KeyIDService service = new KeyIDService("https://keyidservices.tickstream.com", license,0);
+
+        JsonObject data = new JsonObject();
+        Response response = service.Get("Values", data).get();
+
+        String body = response.body().string();
+        assertEquals(body, "[\"value1\",\"value2\"]");
+    }
+
+    @Test
+    public void testPost() throws Exception
+    {
+        String license = readFile("license.txt", StandardCharsets.UTF_8);
+        KeyIDService service = new KeyIDService("https://keyidservices.tickstream.com", license,0);
+
+        JsonObject data = new JsonObject();
+        data.addProperty("tsData", "hZI7bsQwDETPkhtwKP5Ub7sIUu4BcoA06XT40KlMUcDChquHeeaQzsuGjZggHzIiRKeu7/V4rs/nev0/X48PkFRuirGtn53zEiceM0Drd8dsFmx6vvNkxYlr1hqn6qbG3ep2xwxBqnKyRuEYlOxh1hrn0ElvrW5wzbR3Vs88ZJtvrMECy910a6lkklBu42AFKueUSztYa5yLu2u3yh1TooFh1K07JkAcjkmpYAExRU/bMRY/nabeJ1VcSj3c3I6NsHlK44IpC6e0p22YyjDvabUQXIsd6PXWSRHC+T1gZQTO2q6ddqz8W7Y2CB37Aw==" );
+        Response response = service.Post("validation", data).get();
+
+        String body = response.body().string();
+        assertEquals(body, "[{\"Statistic\":\"OpinionFailureCount\",\"Value\":\"3\"},{\"Statistic\":\"OpinionFailureMessages\",\"Value\":\"(7) Text too short (10 min).\\r\\n(43) \\\"hello1234\\\" (1 message) PWD cannot be speed-checked.\\r\\n(7) Text too short (10 min).\"},{\"Statistic\":\"OpinionInfoCount\",\"Value\":\"2\"},{\"Statistic\":\"OpinionInfoMessages\",\"Value\":\"(20) Text contains all or parts of 2 (4-10 letter) common words: hell, hello\\r\\n(20) Text contains all or parts of 2 (4-10 letter) common words: hell, hello\"},{\"Statistic\":\"OpinionWarnCount\",\"Value\":\"10\"},{\"Statistic\":\"OpinionWarnMessages\",\"Value\":\"(19) Text contains easily-keyed sequences (\\\"1234\\\").\\r\\n(22) Text contains too-popular passwords segments (\\\"1234\\\").\\r\\n(22) Text contains too-popular passwords segments (\\\"hello\\\").\\r\\n(27) Text contains excessive number (1) of character type changes.\\r\\n(28) Text contains excessive number (1) of changes between alphas and numerics.\\r\\n(19) Text contains easily-keyed sequences (\\\"1234\\\").\\r\\n(22) Text contains too-popular passwords segments (\\\"1234\\\").\\r\\n(22) Text contains too-popular passwords segments (\\\"hello\\\").\\r\\n(27) Text contains excessive number (1) of character type changes.\\r\\n(28) Text contains excessive number (1) of changes between alphas and numerics.\"}]");
+    }
+
+    @Test
+    public void testTypingMistake() throws Exception
+    {
+        String license = readFile("license.txt", StandardCharsets.UTF_8);
+        KeyIDService service = new KeyIDService("https://keyidservices.tickstream.com", license,0);
+
+        Response response = service.TypingMistake("jgtest1",
+                              "thisismistype",
+                              "session1",
+                              "javatest",
+                              "javatest",
+                              "javatest",
+                              "javatest").get();
+        String body = response.body().string();
+        assertEquals("\"\"", body);
+    }
+
+    @Test
+    public void testNonce() throws Exception
+    {
+        String license = readFile("license.txt", StandardCharsets.UTF_8);
+        KeyIDService service = new KeyIDService("https://keyidservices.tickstream.com", license,0);
+
+        long TICKS_AT_EPOCH = 621355968000000000L;
+        long tick = System.currentTimeMillis()*10000 + TICKS_AT_EPOCH;
+        Response response = service.Nonce(tick).get();
+        String body = response.body().string();
+        assertNotNull(body);
+        assertFalse(body.startsWith("ERR"));
+    }
+
+    @Test
+    public void testRemoveToken() throws Exception
+    {
+        String license = readFile("license.txt", StandardCharsets.UTF_8);
+        KeyIDService service = new KeyIDService("https://keyidservices.tickstream.com", license,0);
+
+        Response response = service.RemoveToken("jgtest@intensityanalytics.com", "").get();
+
+        String body = response.body().string();
+        JsonObject json = new JsonParser().parse(body).getAsJsonObject();
+        String error = json.get("Error").getAsString();
+        assertEquals(error, "");
+    }
+
+    @Test
+    public void testSaveToken() throws Exception
+    {
+        String license = readFile("license.txt", StandardCharsets.UTF_8);
+        KeyIDService service = new KeyIDService("https://keyidservices.tickstream.com", license,0);
+
+        Response response = service.SaveToken("jgtest@intensityanalytics.com", "").get();
+
+        String body = response.body().string();
+        JsonObject json = new JsonParser().parse(body).getAsJsonObject();
+        String error = json.get("Error").getAsString();
+        assertEquals(error, "");
+    }
+}
